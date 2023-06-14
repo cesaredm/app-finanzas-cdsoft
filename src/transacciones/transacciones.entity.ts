a1a1718a043ec8ff9ba58ea01a87f63dc54a1b13
cesaredm <cesardmaradiaga@gmail.com>
2023-06-14 13:09:05 -0600

    .

--- src/transacciones/transacciones.entity.ts
+++ src/transacciones/transacciones.entity.ts
@@ -6,10 +6,10 @@ export class Transacciones {
   id: number;
   @Column({ type: 'datetime', nullable: true })
   fecha: any;
-  @Column({ type: 'varchar', length: 15 })
+  @Column({ type: 'varchar', length: 15, nullable: true })
   tipoMovimiento: string;
-  @Column({ type: 'decimal', precision: 12, scale: 2 })
+  @Column({ type: 'decimal', precision: 12, scale: 2, default: 0 })
   monto: number;
-  @Column({ type: 'varchar', length: 120 })
+  @Column({ type: 'varchar', length: 120, nullable: true })
   nota: string;
 }
